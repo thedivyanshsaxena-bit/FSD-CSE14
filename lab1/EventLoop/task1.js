e29b892dbe7add4350b976f9239800a132edc420
Divyansh Saxena <thedivyanshsaxena@gmail.com>
2026-07-23 15:17:03 +0530

    task2

--- lab1/EventLoop/task1.js
+++ lab1/EventLoop/task1.js
@@ -1,17 +1,25 @@
 function f1 (){
-    console.log('f1');
+    console.log('f1 start');
+    f2();
+    console.log("f1 running");
+    console.log("f1 end");
 };
 function f2 (){
-    console.log('f2');
+    console.log('f2 start');
+    f3();
+    console.log("f2 running");
+    console.log("f2 end");
 };
 function f3 (){
-    console.log('f3');
+    console.log('f3 start');
+    console.log("f3 running");
+    console.log("f3 end");
 };
 
 function main(){
+    console.log("start main");
     f1();
-    f2();
-    f3();
+    console.log("main running");
     console.log("end main");
 }
 main();
