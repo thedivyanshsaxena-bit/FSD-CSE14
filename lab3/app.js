3bcd45dd0855a61aedf7902dcc07d3730907bbda
Divyansh Saxena <thedivyanshsaxena@gmail.com>
2026-08-18 10:35:02 +0530

    app

--- lab3/app.js
+++ lab3/app.js
@@ -1,11 +1,14 @@
 import http from "http";
-const server=http.createServer();
-server.on('request',(req,res)=>{
+
+const server = http.createServer((req, res) => {
+  res.write("<h1>Hello World</h1>");
+  res.end();
+});
+/*server.on('request',(req,res)=>{
     res.write("<h1>Welcome To Server Side Programming</h1>");
     res.write("<h2>Nodemon is tracking the files</h2>");
     res.end();
-});
-
-server.listen(5000,()=>{
-    console.log("Server Is Running")
+});*/
+server.listen(5000, () => {
+  console.log("Server Is Running");
 });
